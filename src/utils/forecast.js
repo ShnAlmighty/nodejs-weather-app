14de2b9a3dbde98de4a2c15675a35b8559c2233a
SHANTANU PATNAIK <patnaikshantanu@gmail.com>
2021-01-17 10:08:37 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (lat,lon,callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=1ab65f91dda55a833f2f6e18980a47a3&query=+'+lat+','+lon;
+    const url = 'http://api.weatherstack.com/current?access_key=YOUR-ACCESS-KEY&query=+'+lat+','+lon;
     request({ url,json:true },(err,{ body }={})=>{
         if(err){
             callback(err,undefined);
