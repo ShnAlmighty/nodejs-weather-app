50d691b9e142f6fcd1d122030f7cab40fb24a1e6
SHANTANU PATNAIK <patnaikshantanu@gmail.com>
2021-01-17 10:09:19 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const geocode = (add,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(add)+'.json?access_token=pk.eyJ1Ijoic2huYWxtaWdodHkiLCJhIjoiY2tkd3lwcHNxMTVxOTMwc2d2cDZqODZhaiJ9.zj5KuvXFfFIUTLjnGLD-WQ&limit=1';
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(add)+'.json?access_token=YOUR-ACCESS-TOKEN&limit=1';
     request({ url, json:true },(err,{ body }={})=>{
         if(err){
             callback('Unable to connect to Map Box',undefined);
